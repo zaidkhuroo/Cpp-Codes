54b7dd1dcdf5313a32b0900caff37f8feba3d8b2
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2022-12-19 19:38:50 +0530

    Add files via upload

---
+++ FunctionOverloading.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void add(int a, int b) //function one with different args
+{
+    cout << "sum =" << (a+b);
+}
+void add(double x, double y) //function 2nd with different args but with same function name as in above
+{
+    cout <<endl <<"sum =" << (x+y);
+}
+
+// main function
+int main()
+{
+    add(10, 20);
+    add(2.2, 4.8);
+    return 0;
+}
