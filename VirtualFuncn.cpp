6d56ea9f684bdb1e3fa10d7e5e4dfa85aeac5843
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2022-12-23 23:39:41 +0530

    Add files via upload

---
+++ VirtualFuncn.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+class hey
+{
+   
+
+public:
+    virtual void red()
+    {
+        cout << "value" << endl;
+       
+    }
+   
+};
+class orange:public hey
+{
+
+public:
+    void red()
+    {
+        cout << "no value" << endl;
+        
+    }
+
+};
+int main(){
+    hey *ptr;
+    orange t;
+    ptr=&t;
+    ptr->red();
+    return 0;
+  
+}
