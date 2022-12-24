a2921bcf12bcd05829e3a02b0a1af4d2275cd10e
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2022-12-24 23:04:23 +0530

    Add files via upload

---
+++ Exceptionhandle2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    try
+    {
+        cout << "i am here" << endl;
+        throw 1;
+        cout << "i am not printed"<<endl;
+    }
+    catch (int x)
+    {
+        cout << "i am finally printed" << endl;
+    }
+    return 0;
+}
