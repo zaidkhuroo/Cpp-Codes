96f6592aee43b1eeed8ad47041578c2ac47f428c
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2022-12-24 23:51:47 +0530

    Add files via upload

---
+++ NestingTryBlock.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+ main()
+{
+     cout << "i am here" << endl;
+    try
+    {
+       
+        throw 100;
+        cout << "i am not printed"<<endl;
+    }
+    catch (int x)
+    {
+        cout << "Finally printed" << endl;
+        throw x;
+    }
+     catch (int y)
+    {
+        cout << "last print" << endl;
+        
+    }
+
+}
