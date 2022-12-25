9421e55f4df1b0a82f07fbb97875524ce6cf220a
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2022-12-25 11:36:08 +0530

    Add files via upload

---
+++ CopyConstructor.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+// void print();
+class stream
+{
+    int a, b;
+
+public:
+    stream(int a, int b) // decalring constructor
+    {
+        (*this).a = a;
+        (*this).b = b;
+    }
+
+    stream(stream &old) // copy constructor
+    {
+        a = old.a;
+        b = old.b;
+    }
+    void print()
+    {
+        cout << "a" << a << endl;
+        cout << "b is" << b << endl;
+      
+};
+int main()
+{
+    stream s1(21, 20);
+    stream s2(s1);
+    s1.print();
+    s2.print();
+    return 0;
+}
