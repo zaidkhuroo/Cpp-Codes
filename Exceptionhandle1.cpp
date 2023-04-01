420471b84f253e079b627856c57a7be2ed6f4ec6
Zaid Bin Farooq <108170101+zaidkhuroo@users.noreply.github.com>
2023-04-01 13:27:00 +0530

    Update Exceptionhandle1.cpp

--- Exceptionhandle1.cpp
+++ Exceptionhandle1.cpp
@@ -1,16 +1,20 @@
+// adding header files
 #include<iostream>
 using namespace std;
 int main(){
 
     int a,b;
-    try
+//  trying an integer 
+    try 
     {    
         cout<<"enter two numbers"<<endl;
         cin>>a>>b;
+//         throws if the integer value is 0
     if(b==0) throw 0;
     
 cout<<"divsion of a and b is "<<a/b<<endl;
 }
+// as the integer =0 so division with 0 would'nt take place
 catch(int x)
 {
 cout<<"Sorry division is not possible"<<endl;
